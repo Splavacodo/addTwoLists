f124ff3f62aea5516ff6d4099ad17a7ed8a2005c
Jason Zamora <52217987+Splavacodo@users.noreply.github.com>
2021-02-11 23:43:11 -0700

    Create Solution.js

---
+++ Solution.js
@@ -0,0 +1,28 @@
+let addTwoNumbers = (l1, l2) => {
+    //create a variable sum which keeps track of the sum of one single index of both lists
+    let sum = 0;
+    let current = new ListNode(0); 
+    let result = current; 
+    
+    while(l1 || l2) {
+        if(l1) {
+            sum += l1.val; 
+            l1 = l1.next; 
+        }
+        
+        if(l2) {
+            sum += l2.val; 
+            l2 = l2.next; 
+        }
+        
+        current.next = new ListNode(sum % 10); 
+        current = current.next; 
+        sum = sum > 9 ? 1 : 0;
+    }
+    
+    if(sum) {
+        current.next = new ListNode(sum); 
+    }
+    
+    return result.next; 
+};
