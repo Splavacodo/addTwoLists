4e97c77becf16e13f3274b8976d3bc5b67f50922
Jason Zamora <52217987+Splavacodo@users.noreply.github.com>
2021-02-13 00:00:22 -0700

    Update Solution.js

--- Solution.js
+++ Solution.js
@@ -1,7 +1,7 @@
 let addTwoNumbers = (l1, l2) => {
     //create a variable sum which keeps track of the sum of one single index of both lists
     let sum = 0;
-    let current = new ListNode(0); 
+    let current = new ListNode(0); //current will add up with .next and be returned as result
     let result = current; 
     
     while(l1 || l2) {
